9833392a421aa858242893f73c5a4c0fee4958d3
혜리 <phl0615@naver.com>
2024-07-06 18:47:54 +0900

    Feat: 더미 데이트 파일

---
+++ referto-app/src/data/references.js
@@ -0,0 +1,8 @@
+const references = [
+  { paperInfo_id: "1", reference: "하나(2020)." },
+  { paperInfo_id: "2", reference: "둘(2021)." },
+  { paperInfo_id: "3", reference: "셋(2022)." },
+  { paperInfo_id: "4", reference: "넷(2023)." },
+];
+
+export default references;
