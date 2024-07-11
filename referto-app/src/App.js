e6bdfe2ea0c0c5cf1492a36ebc470580884dafc3
eunjae <eunjae1004@snu.ac.kr>
2024-07-11 16:59:08 +0900

    Feat: 과제 선택 에 러 해결 및 과제 삭제 보완, ReferenceDetailPage의 참고문헌 삭제 및 수정 에러 해결 및 기능 보완, 참고문헌 변경시 변경된 순서에 맞게 참고문헌 번호 바뀌도록 구현

--- referto-app/src/App.js
+++ referto-app/src/App.js
@@ -1,10 +1,44 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useState, useCallback } from "react"
 import Header from "./components/Header";
 import HomePage from "./routes/HomePage";
 import ReferenceDetailPage from "./routes/ReferenceDetailPage";
+import references from "./data/references.js"
 import "./App.css";
 
 function App() {
+  const [referencesList, setReferencesList] = useState(references);
+  const findIndexofReference = (referenceId) => {
+    const index = referencesList.findIndex(reference => reference.paperInfo_id === referenceId);
+    return index;
+  }
+  const handleReferenceDelete = (referenceId, event) => {
+    if (window.confirm("Do you really want to delete?")){
+      setReferencesList(
+        referencesList.filter(
+          (reference) => reference.paperInfo_id !== referenceId
+        )
+      ); 
+    } else {
+      event.preventDefault();
+    }
+  };
+
+  const handleReferenceUpdate = (referenceId, newContent) => {
+    setReferencesList(
+      referencesList.map((reference) => {
+        if (reference.paperInfo_id === referenceId) {
+          return { ...reference, reference: newContent };
+        }
+        return reference;
+      })
+    );
+  };
+
+  const getAllReferences = useCallback(() => {
+    return referencesList.map((ref) => ref.reference);
+  }, [referencesList]);
+  //referencesList에서 reference각주부분만 가져와서 리스트로 만듦.
   return (
     <div className="App">
       <BrowserRouter>
@@ -12,9 +46,24 @@ function App() {
         <Routes>
           <Route
             path="/:assignmentId/:referenceId"
-            element={<ReferenceDetailPage />}
+            element={
+            <ReferenceDetailPage
+              referencesList={referencesList}
+              handleReferenceDelete={handleReferenceDelete}
+              handleReferenceUpdate={handleReferenceUpdate}
+              findIndexofReference={findIndexofReference}
+            />}
           />
-          <Route path="/:assignmentId" element={<HomePage />} />
+          <Route 
+            path="/:assignmentId" 
+            element={
+            <HomePage 
+              referencesList={referencesList}
+              handleReferenceDelete={handleReferenceDelete}
+              handleReferenceUpdate={handleReferenceUpdate}
+              getAllReferences={getAllReferences}
+              findIndexofReference={findIndexofReference}
+            />} />
           <Route path="/" element={<Navigate to="/1" />} />
         </Routes>
       </BrowserRouter>
