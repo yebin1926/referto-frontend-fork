bc85a933eb13bc574e9e069caa7e639fde4df28c
Hypell <phl0615@snu.ac.kr>
2024-07-27 00:30:29 +0900

    Fix: console log 지우기

--- referto-app/src/routes/DetailPage.jsx
+++ referto-app/src/routes/DetailPage.jsx
@@ -19,13 +19,6 @@ const DetailPage = () => {
     referencesList,
   } = location.state || {};
 
-  console.log(referenceName)
-  console.log(referenceName)
-  console.log(referenceName)
-  console.log(referenceName)
-  console.log(referenceName)
-  console.log("aaaaaaaa")
-
   const [paperUrl, setPaperUrl] = useState(null);
   const [content, setContent] = useState(referenceName);
   const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth < 1100);
@@ -59,26 +52,6 @@ const DetailPage = () => {
     if (index > 1) {
       const newReference = referencesList[index - 1];
       const newReferenceId = newReference["paperInfo_id"];
-
-      console.log(referencesList)
-      console.log(referencesList)
-      console.log(referencesList)
-      console.log(referencesList)
-      console.log(referencesList)
-
-      console.log(index)
-      console.log(index)
-      console.log(index)
-      console.log(index)
-      console.log(index)
-
-      console.log(newReference[selectedStyleName])
-      console.log(newReference[selectedStyleName])
-      console.log(newReference[selectedStyleName])
-      console.log(newReference[selectedStyleName])
-      console.log(newReference[selectedStyleName])
-      console.log(newReference[selectedStyleName])
-
 
       navigate(`/${assignmentId}/${newReferenceId}`, {
         state: {
