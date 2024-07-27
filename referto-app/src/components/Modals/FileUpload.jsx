e43d4a17659b000f702526916fb6368b899859c2
eunjae <eunjae1004@snu.ac.kr>
2024-07-27 10:17:03 +0900

    Fix: 사이드바 이름 수정시 리로드, 파일업로드시 에러 콘솔

--- referto-app/src/components/Modals/FileUpload.jsx
+++ referto-app/src/components/Modals/FileUpload.jsx
@@ -40,9 +40,11 @@ const FileUploadModal = ({ setIsOpen }) => {
 
         try {
           const response_paper = await uploadPaper(formData, config);
+          console.log("response_paper 통과");
           const response_paperinfo = await uploadPaperInfo(
             response_paper.data.paper_id
           );
+          console.log("response_paperinfo 통과");
           //await createMemo(response_paper.data.paper_id);
         } catch (error) {
           console.error("1번째 Error during file processing:", error.message);
